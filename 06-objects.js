8e384b8b29d383f594e4c2ce510df3335475967e
magas1507 <asmg15.07@gmail.com>
2024-02-19 07:22:56 -0300

    [M1S04] - Ex 6 - Objetos

--- 06-objects.js
+++ 06-objects.js
@@ -1,33 +1,30 @@
+const person1 = {
+  nome: "Gabriel",
+  sobrenome: "Carrillo",
+  idade: 29,
+  profissao: "Programador"
+};
 
-  
-  // Objet 1
-  const person1 = {
-    nome: "Gabriel",
-    sobrenome: "Carrillo",
-    idade: 29,
-    profissao: "Programador"
-  };
-  
-  console.log(person1) 
-  
-  // Objeto 2
-  const person2 = {
-    nome: "Maria",
-    sobrenome: "Acosta",
-    idade: 35,
-    profissao: "Bioanalista"
-  };
-  let person2String = JSON.stringify(person2)
-  console.log(person2String);
+console.log(person1)
 
-  // Objeto 3
-  const person3 = {
-    nome: "Hector",
-    sobrenome:"Flores",
-    idade: 32,
-    profissao: "Lic Ciência da Computação"
-  };
-  
+
+const person2 = {
+  nome: "Maria",
+  sobrenome: "Acosta",
+  idade: 35,
+  profissao: "Bioanalista"
+};
+let person2String = JSON.stringify(person2)
+console.log(person2String);
+
+
+const person3 = {
+  nome: "Hector",
+  sobrenome: "Flores",
+  idade: 32,
+  profissao: "Lic Ciência da Computação"
+};
+
 for (let prop in person3) {
-    console.log(`${prop}: ${person3[prop]}`)
+  console.log(`${prop}: ${person3[prop]}`)
 }
