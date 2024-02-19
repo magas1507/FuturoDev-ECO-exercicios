fffaea61d4284b7563093ec610ea1ec4b7e7ff2b
magas1507 <asmg15.07@gmail.com>
2024-02-18 23:04:04 -0300

    [M1S04] - Ex 5 -Estruturas condicionais

---
+++ 05-condicional-structures.js
@@ -0,0 +1,10 @@
+function verifyingNumber(numbers) {
+	if(numbers>0){
+		console.log('O número é positivo');
+	}else if(numbers<0){
+		console.log('O número é negativo');
+	}else{
+		console.log('o número é zaero')
+	} 
+}
+
