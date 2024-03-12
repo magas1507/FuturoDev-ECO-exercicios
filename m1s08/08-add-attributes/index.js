d72f6761e0a245d7892efdda08cee7ed119f6a32
magas1507 <asmg15.07@gmail.com>
2024-03-12 08:12:40 -0300

    [M1S08] - Ex 8 - Adicionar Atributo

---
+++ m1s08/08-add-attributes/index.js
@@ -0,0 +1,6 @@
+function addAttribute(){
+    console.log("entro")
+    let element = document.getElementById("meuElemento");
+    
+    element.setAttribute("style", "background-color: yellow; color: black; font-weight: bold;");
+}    
