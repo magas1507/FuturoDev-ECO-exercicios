8391f15b05a774ce7ba88f504caac9562d440577
magas1507 <asmg15.07@gmail.com>
2024-03-13 20:55:03 -0300

    [M1S05] Ex 1 - Desenvolva uma tela para simular consulta de preços e compra de produtos de mercado

---
+++ m1s08/11-list-product/index.js
@@ -0,0 +1,16 @@
+function consultarProduto() {
+	console.log('Consulta nome do producto')
+}
+
+function comprarProduto() {
+	console.log('Comprar produto')
+}
+
+function simularConsultaPreco(produto) {
+	console.log('Consultar preço')
+}
+
+
+function simularCompraProduto(produto) {
+	console.log('comprar produto consultado')
+}
