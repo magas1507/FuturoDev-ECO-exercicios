306afaeb409f53940b954c8794bf952f3b8ebe00
magas1507 <asmg15.07@gmail.com>
2024-04-14 20:56:35 -0300

    Ex. 4 - Crie o componente de header da aplicação

--- m1s11/adveture-trail-fd/src/App.jsx
+++ m1s11/adveture-trail-fd/src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
 import CardTrilha from "./components/CardTrilha/index.jsx";
 import useFetch from "./hooks/useFetch.jsx";
+import Header from "./components/Header/index.jsx";
 
 function App() {
   const [data, loading] = useFetch("public/data/data.json");
 
   return (
     <>
+      <Header />
       {loading && <h4>Carregando trilhas</h4>}
       {!loading && data && (
         <>
-          {data.map((trilhas, index) => (
-            <CardTrilha dadosTrilha={trilhas} key={index} />
-          ))}
+          <div className="container">
+            {data.map((trilhas, index) => (
+              <CardTrilha dadosTrilha={trilhas} key={index} />
+            ))}
+          </div>
         </>
       )}
     </>
